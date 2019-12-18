2beb6111c45e29cc1c6a2e77d51f1d686db1b437
Daniel Narey <danielnarey@users.noreply.github.com>
2019-12-17 23:34:42 -0500

    update color-system

--- src/color-params.js
+++ src/color-params.js
@@ -2,11 +2,7 @@ import colorSystem from '@metamodern/color-system';
 
 
 const colorParams = (options = {}) => ({
-  colors: colorSystem(options).theme,
-  borderColor: (t) => ({
-    default: options.defaultBorderColor || 'transparent',
-    ...t('colors'),
-  }),
+  ...colorSystem(options).theme,
   placeholderColor: {},
 });
 
