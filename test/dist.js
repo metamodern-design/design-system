df332d7f1506db0e5982905c688b9fad752af141
Daniel Narey <danielnarey@users.noreply.github.com>
2020-06-04 15:58:39 -0400

    fix tests

--- test/dist.js
+++ test/dist.js
@@ -7,7 +7,7 @@ const defaultSystem = designSystem();
 test(`dist: outer keys were generated`, (t) => {
   t.deepEqual(
     Object.keys(defaultSystem).sort(),
-    ['theme', 'variants'].sort(),
+    ['theme', 'variants', 'corePlugins'].sort(),
   );
 });
 
@@ -22,12 +22,5 @@ test(`dist: theme.extend keys were generated`, (t) => {
   t.deepEqual(
     Object.keys(defaultSystem.theme.extend).sort(),
     ['fontWeight', 'fontFamily'].sort(),
-  );
-});
-
-test(`dist: variants keys were generated`, (t) => {
-  t.deepEqual(
-    Object.keys(defaultSystem.variants).sort(),
-    ['accessibility', 'alignContent', 'alignItems', 'alignSelf', 'appearance', 'backgroundAttachment', 'backgroundColor', 'backgroundPosition', 'backgroundRepeat', 'backgroundSize', 'borderCollapse', 'borderColor', 'borderRadius', 'borderStyle', 'borderWidth', 'boxShadow', 'cursor', 'display', 'fill', 'flex', 'flexDirection', 'flexGrow', 'flexShrink', 'flexWrap', 'float', 'fontFamily', 'fontSize', 'fontSmoothing', 'fontStyle', 'fontWeight', 'height', 'inset', 'justifyContent', 'letterSpacing', 'lineHeight', 'listStylePosition', 'listStyleType', 'margin', 'maxHeight', 'maxWidth', 'minHeight', 'minWidth', 'objectFit', 'objectPosition', 'opacity', 'order', 'outline', 'overflow', 'padding', 'placeholderColor', 'pointerEvents', 'position', 'resize', 'stroke', 'tableLayout', 'textAlign', 'textColor', 'textDecoration', 'textTransform', 'userSelect', 'verticalAlign', 'visibility', 'whitespace', 'width', 'wordBreak', 'zIndex'].sort(),
   );
 });
